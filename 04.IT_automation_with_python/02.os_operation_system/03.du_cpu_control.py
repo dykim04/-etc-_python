e69b855ee916a20a7359f38d702437ce3c748653
dykim04 <dykim9304@gmail.com>
2023-03-06 03:28:32 +0900

    du

---
+++ 04.IT_automation_with_python/02.os_operation_system/03.du_cpu_control.py
@@ -0,0 +1,44 @@
+import shutil
+import psutil
+
+# duplicate the linux du function
+du = shutil.disk_usage("/")
+print(du)
+
+print(du.free/du.total)
+print((du.free/du.total) * 100)
+
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+print(psutil.cpu_percent(0.1))
+
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+print(psutil.cpu_percent(0.5))
+
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+print(psutil.cpu_percent(1.0))
+
+
