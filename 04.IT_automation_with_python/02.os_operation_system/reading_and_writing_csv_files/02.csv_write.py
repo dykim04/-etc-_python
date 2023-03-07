3d1fc8aabc9b44ef822fee165d43e16f9cc1040c
dykim04 <dykim9304@gmail.com>
2023-03-08 04:28:53 +0900

    csv_file_read/write

--- 04.IT_automation_with_python/02.os_operation_system/reading_and_writing_csv_files/02.csv_write.py
+++ 04.IT_automation_with_python/02.os_operation_system/reading_and_writing_csv_files/02.csv_write.py
@@ -1,13 +1,16 @@
-# importing modules
+# import modules
 import csv
 import os
 
 # target directory
 os.chdir("C:/Users/dy/Default/ansel/Desktop/dy/1.computer_science/cs_knowledge/python/04.IT_automation_with_python/02.os_operation_system/reading_and_writing_csv_files")
 
+# if csv_file directory does not exists -> create one
+if os.path.isdir("./csv_files") == False:
+    os.mkdir("./csv_files")   
+
 # csv data
-hosts = [
-    
+hosts = [    
     ["workstation.local", "192.168.25.46"], 
     ["webserver.cloud", "10.2.5.6"],
     ["workstation.local", "192.168.25.46"], 
@@ -16,16 +19,15 @@ hosts = [
     ["webserver.cloud", "10.2.5.6"],
     ["workstation.local", "192.168.25.46"], 
     ["webserver.cloud", "10.2.5.6"]
-    
-    ]
+]
 
 # writerows method
-with open("hosts.csv", "w", newline = "") as hosts_csv:
+with open("./csv_files/hosts.csv", "w", newline="") as hosts_csv:
     writer = csv.writer(hosts_csv)
     writer.writerows(hosts)
 
 # writerow method
-with open("hosts2.csv", "w", newline = "") as hosts2_csv:
+with open("./csv_files/hosts2.csv", "w", newline="") as hosts2_csv:
     writer = csv.writer(hosts2_csv)
     for element in hosts:
         writer.writerow(element)
